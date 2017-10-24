b180d81802b9c1f6bacb16fa3e304831d9de3c10
Olivia Marunde <omarunde8029@jh-mba158-10.d214.org>
2017-10-24 09:01:55 -0500

    Initial Commit

---
+++ Cloud Kit CPB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cloud Kit CPB
+//
+//  Created by Olivia Marunde on 10/24/17.
+//  Copyright © 2017 Olivia Marunde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
